97bb7870c45eef08c4c44a3238333103c602ca8e
imranmsoft <mohammad.imran@dsinnovators.com>
2022-01-09 19:36:52 +0600

    Spring boot Docker Image

---
+++ src/test/java/com/imran/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imran.spring.springselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
